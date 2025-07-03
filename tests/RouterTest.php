5351e05be0d221cbfd46313af8bf5d691486fc23
lukehayes <lukepurplehayes@gmail.com>
2025-07-03 09:10:35 +0100

    test: add testCanSplitRequestUri()

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -11,6 +11,17 @@ final class RouterTest extends TestCase
 
     protected function setUp() : void
     {
-        $this->routerObj = new Router();
+        //$this->routerObj = new Router();
+    }
+
+    public function testCanSplitRequestUri()
+    {
+        $_SERVER['REQUEST_URI'] = 'controller/action';
+
+        $router = new Router();
+        $uri_sections = $router->splitRequestUri();
+
+        $this->assertContains('controller', $uri_sections);
+        $this->assertContains('action', $uri_sections);
     }
 }
